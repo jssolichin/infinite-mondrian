7b5dd899d3894c817287b259a86b7bc621beb5d7
Jonathan <jssolichin@gmail.com>
2014-12-01 15:09:24 -0800

    Add bars. Resolution dependent box spread/size.

--- js/box.js
+++ js/box.js
@@ -1,10 +1,9 @@
 var Box = function (shared){
 
     this.generateSize(shared.width, shared.height, shared.depth);
-    this.color = this.generateColor();
+    this.color = this.generateColor(shared.multicolor);
 
     this.position = {x: 0, y: 0, z:0};
-
 
     var geometry = new THREE.BoxGeometry( this.width, this.height, this.depth);
     var material = new THREE.MeshLambertMaterial( {color: this.color} );
@@ -12,15 +11,20 @@ var Box = function (shared){
 
     this.mesh = mesh.clone();
 };
-Box.prototype.generateColor = function (){
-    var colors  = [0xff0000, 0x0000ff, 0xffff00];
+Box.prototype.generateColor = function (multicolor){
+    var colors;
+    if(multicolor)
+        colors  = [0xff0000, 0x0000ff, 0xffff00];
+    else
+        colors = [0x000000];
+
     return colors[Math.floor(Math.random() * colors.length)];
 }
 Box.prototype.generatePosition = function (cameraPos) {
     var that = this;
 
     var components = ['x', 'y', 'z'];
-    var bounds  = 1000;
+    var bounds  = WIDTH*3.5;
 
     components.forEach(function(component){
         var min = cameraPos[component] - bounds;
@@ -39,4 +43,5 @@ Box.prototype.generateSize = function (width, height, depth) {
     this.height = helpers.generateRandom(height/4, height);
     this.depth = helpers.generateRandom(depth/4, depth);
 };
+
 
--- js/main.js
+++ js/main.js
@@ -45,10 +45,12 @@ var init = function () {
 
     //create subtle ambient light
     var ambientLight = new THREE.AmbientLight(0x333355);
+     ambientLight = new THREE.AmbientLight(0x111111);
 
     //create a scene
     shared.scene = new THREE.Scene();
-    
+    shared.scene.fog = new THREE.Fog(0xffffff, 0, WIDTH*5);
+
     // add the camera to the scene
 
     shared.scene.add(control.getObject());
@@ -56,21 +58,44 @@ var init = function () {
     shared.scene.add(ambientLight);
 
 
-    //add boxes
+    //param for boxes
     boxOption = {
-        width: WIDTH/5, height: HEIGHT/5, depth: WIDTH/5
+        width: WIDTH/2, height: HEIGHT/2, depth: WIDTH/2, multicolor: true
+    };
+
+    //param for bar--function generates which dimension to make really short and long
+    barOptionGenerator = function(){
+        //make all dimension short. no multicolor since only black
+        var barOption = {
+            width: WIDTH/15, height: HEIGHT/15, depth: WIDTH/15, multicolor: false
+        };
+
+        //choose which dimension to make really long
+        var dimensions = ['width', 'height', 'depth'];
+        barOption[dimensions[helpers.generateRandom(0,2)]] = 99999;
+
+        return barOption;
     }
 
+    //list of objects in world
     var boxes = [];
-    for (var i = 0; i< 100; i++){
-        boxes[i] =  new Box(boxOption);
+    for (var i = 0; i< 400; i++){
+        var type; //bar or box
+
+        //create a bar some percent of the time, else boxes
+        if(Math.random() > .8)
+            type = barOptionGenerator();
+        else
+            type = boxOption;
+
+        //add object to scene
+        boxes[i] =  new Box(type);
         shared.scene.add(boxes[i].mesh);
 
+        //move to a position in world
         boxes[i].generatePosition(shared.camera.position);
         boxes[i].move();
     }
-    //box.generatePosition(shared.camera.position);
-    //box.move();
 
     // attach the render-supplied DOM element
     $container.appendChild(shared.renderer.domElement);
@@ -79,10 +104,12 @@ var init = function () {
 var anim = function () {
 
     stats.begin();
+
     requestAnimationFrame(anim);
 
+    //move camera overtime slowly
     shared.camera.position.z--;
-    shared.renderer.render(shared.scene, shared.camera)
+    shared.renderer.render(shared.scene, shared.camera);
 
     stats.end();
 }
