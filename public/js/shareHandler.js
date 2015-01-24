c516044e040247d988c1fe3321c479f8541e00a3
Jonathan <jssolichin@gmail.com>
2015-01-23 22:29:36 -0800

    Sharehandler updated to have correct information

--- public/js/shareHandler.js
+++ public/js/shareHandler.js
@@ -6,23 +6,22 @@ var shareSettings = function (data){
     return {
         networks: {
             facebook: {
-
+				load_sdk: true,
+					app_id: '817816641608595'
             },
             google_plus:{
-
             },
             twitter:{
-
+                description: 'I made a mondrian!', //needs own description because char limit
+                url: url.origin+'/'+data.img
             },
             pinterest: {
-
             },
             email:{
-
             }
         },
-        image: 'http://localhost:3000/'+data.img,
-        url: 'http://localhost:3000/'+data.img,
+        image: url.origin+'/'+data.img,
+        url: url.origin+'/'+data.img,
         title: 'Check out this Mondrian Work I captured!',
         description: 'I tapped into my inner artist and went into the world DeStjil #infinitemondrian'
     }
